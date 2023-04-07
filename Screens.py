4108951af6e731ac960f2fc703c36365a24ddac6
Subhranil Sarkar <subhranil.sarkar2605@gmail.com>
2023-04-07 21:59:07 +0530

    Modify the main file
    Add jpeg in filtering path, path attribute to default opening directory, root.select() is added in FileChooserIconView
    And fix 'id: "my_image"' -> 'id: my_image'

--- Screens.py
+++ Screens.py
@@ -1,60 +1,64 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
+
 Builder.load_string("""
 <MenuScreen>:
     BoxLayout:
         orientation:"vertical"
         Label:
-            text:"oral cancer daignosis"
-            pos_hint:{'center_x':0.5, 'center_y':0.9}
+            text: "Oral Cancer Diagnosis"
+            pos_hint: {'center_x':0.5, 'center_y':0.9}
             theme_text_color: "Primary"
-            text_color:'black'
-            font_style:'H3'
-            font_size:'25sp'
+            text_color: 'black'
+            font_style: 'H3'
+            font_size: '25sp'
         Button:
             text: 'Goto settings'
             on_press: root.manager.current = 'settings'
 
 <SettingsScreen>:
     BoxLayout:
-        size:root.width,root.height
+        size:root.width, root.height
+        
         FileChooserIconView: 
             id: filechooser
-            filters: ['*.png', '*.jpg']
-            on_selection: selected(filechooser.selection)
+            filters: ['*.png', '*.jpg', '*jpeg']
+            path: '/home/sdevdata/Pictures'
+            on_selection: root.selected(filechooser.selection)
+            
         Image:
-            id:"my_image"
+            id: my_image
             source:""
 
             
 """)
 
+
 # Declare both screens
 class MenuScreen(Screen):
     pass
 
+
 class SettingsScreen(Screen):
-    def selected(self,filename):
+    def selected(self, filename):
         try:
-            self.ids.my_image.source=filename[0]
+            self.ids.my_image.source = filename[0]
         except:
             pass
-    
-    pass
+
 
 class TestApp(App):
 
     def build(self):
+
         # Create the screen manager
         sm = ScreenManager()
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(SettingsScreen(name='settings'))
 
         return sm
+
 
 if __name__ == '__main__':
     TestApp().run()
